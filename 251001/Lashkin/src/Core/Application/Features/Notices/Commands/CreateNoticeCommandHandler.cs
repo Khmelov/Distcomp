67788fa9a9d053185404231e624fc354e328399a
vlados1kin <lashkin2004@gmail.com>
2025-02-11 23:09:25 +0300

    Add notices commands and queries

---
+++ 251001/Lashkin/src/Core/Application/Features/Notices/Commands/CreateNoticeCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTO.Response;
+using AutoMapper;
+using Domain.Repositories;
+using MediatR;
+using Domain.Entities;
+
+namespace Application.Features.Notices.Commands;
+
+public class CreateNoticeCommandHandler : IRequestHandler<CreateNoticeCommand, NoticeResponseTo>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CreateNoticeCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+    
+    public async Task<NoticeResponseTo> Handle(CreateNoticeCommand request, CancellationToken cancellationToken)
+    {
+        var notice = _mapper.Map<Notice>(request.NoticeRequestTo);
+        
+        _unitOfWork.Notice.Create(notice);
+
+        await _unitOfWork.SaveChangesAsync();
+        
+        var noticeResponseTo = _mapper.Map<NoticeResponseTo>(notice);
+        
+        return noticeResponseTo;
+    }
+}
