450a5fecd63b43ddeb51a0f433fe3b504a52ae92
vlados1kin <lashkin2004@gmail.com>
2025-02-11 23:11:04 +0300

    Add exception handling middleware

--- 251001/Lashkin/src/Presentation/API/Program.cs
+++ 251001/Lashkin/src/Presentation/API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middlewares;
 using Application.Extensions;
 using Persistence.Extensions;
 
@@ -9,11 +10,14 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options => options.AddPolicy("RestCors", policyBuilder => policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 builder.Services.ConfigureRepositories();
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 builder.Services.ConfigureMapper();
 builder.Services.ConfigureMediatr();
 builder.Services.ConfigureValidation();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
