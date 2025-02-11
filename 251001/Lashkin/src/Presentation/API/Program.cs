ee8118565be18a7a8ff3b71c34b2c55524ba86cd
vlados1kin <lashkin2004@gmail.com>
2025-02-11 23:08:23 +0300

    Add automapper and mediatr

--- 251001/Lashkin/src/Presentation/API/Program.cs
+++ 251001/Lashkin/src/Presentation/API/Program.cs
@@ -1,10 +1,24 @@
+using Application.Extensions;
 using Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy("RestCors", policyBuilder => policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 builder.Services.ConfigureRepositories();
+builder.Services.ConfigureMapper();
+builder.Services.ConfigureMediatr();
 builder.Services.ConfigureValidation();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("RestCors");
 
 app.Run();
