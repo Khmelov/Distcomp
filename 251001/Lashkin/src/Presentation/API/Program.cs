78bc62f2fcada0d6b0ee498415ae6258f8f8b7f9
vlados1kin <lashkin2004@gmail.com>
2025-02-11 23:07:11 +0300

    Add validators

--- 251001/Lashkin/src/Presentation/API/Program.cs
+++ 251001/Lashkin/src/Presentation/API/Program.cs
@@ -3,6 +3,7 @@ using Persistence.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureRepositories();
+builder.Services.ConfigureValidation();
 
 var app = builder.Build();
 
