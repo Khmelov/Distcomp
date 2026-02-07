335790420b7eab3abc7ad8ec78d9d29b3d0ddf1e
Vitali Zhuravski <vitaliycof@outlook.com>
2026-02-08 01:22:43 +0300

    Распилил Endpoints.

---
+++ 351003/zhuravski/ArticleHouse/Endpoints/ArticleEndpoints.cs
@@ -0,0 +1,22 @@
+using ArticleHouse.Service.Interface.Article;
+
+namespace ArticleHouse.Endpoints;
+
+public static class ArticleEndpoints
+{
+    private static readonly string GroupPrefix = "/api/v1.0/articles";
+
+    public static void MapArticleEndpoints(this IEndpointRouteBuilder app)
+    {
+        var articleGroup = app.MapGroup(GroupPrefix).WithParameterValidation();
+        articleGroup.MapGet("/", async (IArticleService service) =>
+        {
+            return Results.Ok(await service.GetAllArticlesAsync());
+        });
+        articleGroup.MapPost("/", async (IArticleService service, ArticleRequestDTO dto) =>
+        {
+            ArticleResponseDTO result = await service.CreateArticleAsync(dto);
+            return Results.Created($"/articles/{result.Id}", result);
+        });
+    }
+}
--- 351003/zhuravski/ArticleHouse/Program.cs
+++ 351003/zhuravski/ArticleHouse/Program.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using ArticleHouse;
 using ArticleHouse.ExcMiddleware;
-using ArticleHouse.Service.Interface.Article;
+using ArticleHouse.Endpoints;
 using ArticleHouse.Service.Interface.Creator;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,50 +27,7 @@ app.MapGet("/", async (HttpContext context) =>
     await context.Response.WriteAsync("<h1>Main page</h1>\n<img src=\"http://localhost:24110/static/img.jpg\">");
 });
 
-var v1Group = app.MapGroup("/api/v1.0");
-var creatorGroup = v1Group.MapGroup("/creators").WithParameterValidation();
-creatorGroup.MapGet("/", async (ICreatorService service) =>
-{
-    return Results.Ok(await service.GetAllCreatorsAsync());
-});
-creatorGroup.MapPost("/", async (ICreatorService service, CreatorRequestDTO dto) =>
-{
-    CreatorResponseDTO responseDTO = await service.CreateCreatorAsync(dto);
-    return Results.Created($"/creators/{responseDTO.Id}", responseDTO);
-});
-creatorGroup.MapDelete("/{id}", async (ICreatorService service, long id) =>
-{
-    await service.DeleteCreatorAsync(id);
-    return Results.NoContent();
-});
-creatorGroup.MapGet("/{id}", async (ICreatorService service, long id) =>
-{
-    return Results.Ok(await service.GetCreatorByIdAsync(id));
-});
-//Колхоз - и я не одобряю.
-//Qwen тоже не одобряет.
-creatorGroup.MapPut("/", async (ICreatorService service, CreatorRequestDTO dto) =>
-{
-    if (null == dto.Id)
-    {
-        throw new ValidationException("Creator identifier is missing.");
-    }
-    return Results.Ok(await service.UpdateCreatorByIdAsync((long)dto.Id, dto));
-});
-creatorGroup.MapPut("/{id}", async (ICreatorService service, CreatorRequestDTO dto, long id) =>
-{
-    return Results.Ok(await service.UpdateCreatorByIdAsync(id, dto));
-});
-
-var articleGroup = v1Group.MapGroup("/articles").WithParameterValidation();
-articleGroup.MapGet("/", async (IArticleService service) =>
-{
-    return Results.Ok(await service.GetAllArticlesAsync());
-});
-articleGroup.MapPost("/", async (IArticleService service, ArticleRequestDTO dto) =>
-{
-    ArticleResponseDTO result = await service.CreateArticleAsync(dto);
-    return Results.Created($"/articles/{result.Id}", result);
-});
+app.MapCreatorEndpoints();
+app.MapArticleEndpoints();
 
 app.Run();
