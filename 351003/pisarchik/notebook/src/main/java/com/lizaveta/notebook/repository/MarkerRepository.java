2ab1c7e3a14af87513b016b88fa30abc54986da4
ririgly.4@gmail.com <Elisabeth0321>
2026-02-08 20:48:01 +0300

    task1 done

---
+++ 351003/pisarchik/notebook/src/main/java/com/lizaveta/notebook/repository/MarkerRepository.java
@@ -0,0 +1,9 @@
+package com.lizaveta.notebook.repository;
+
+import com.lizaveta.notebook.model.entity.Marker;
+
+/**
+ * Repository interface for Marker entity.
+ */
+public interface MarkerRepository extends CrudRepository<Marker, Long> {
+}
