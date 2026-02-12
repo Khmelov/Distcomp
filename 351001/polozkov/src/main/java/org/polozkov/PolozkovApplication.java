84caa48cc2525abfae268f962df7b82c87cc97d5
Dj474 <toxap06@gmail.com>
2026-02-12 19:38:45 +0300

    feat: add postgres

--- 351001/polozkov/src/main/java/org/polozkov/PolozkovApplication.java
+++ 351001/polozkov/src/main/java/org/polozkov/PolozkovApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication()
 public class PolozkovApplication {
 
 	public static void main(String[] args) {
