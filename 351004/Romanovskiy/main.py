7fa3a11fe646f1dbfe509a99a63563f615f42ed7
matveq77 <matveuxdqwerty@gmail.com>
2026-02-15 21:30:33 +0300

    done

---
+++ 351004/Romanovskiy/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+from src.api.v1 import router_v1
+from src.core.errors import register_error_handlers
+
+app = FastAPI(title="DistComp", version="1.0")
+
+register_error_handlers(app)
+
+app.include_router(router_v1, prefix="/api")
