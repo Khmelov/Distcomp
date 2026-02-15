8d49c4af0b85249fbb179087e4558be47ffc0587
playerz0redd <ipasha1337@yandex.by>
2026-02-15 18:54:52 +0300

    refact lab 1

--- 351004/Bychkowski/Lab1/main.py
+++ 351004/Bychkowski/Lab1/main.py
@@ -8,10 +8,8 @@ app = FastAPI(title="Task310 REST API")
 
 app.include_router(router)
 
-# Регистрируем обработчики ошибок
 app.add_exception_handler(AppError, app_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 if __name__ == "__main__":
-    # Запуск сервера на порту 24110 согласно ТЗ
     uvicorn.run("main:app", host="0.0.0.0", port=24110, reload=True)
