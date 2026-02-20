eb653c0c31c22e9cd758a35b4d47ae6caa96ea5b
Alexandr Mikhalkov <138939399+zuluw@users.noreply.github.com>
2026-02-20 12:11:31 +0300

    Auto-merge PR #577 (Automated)

---
+++ 351002/Mikhalkov/Distcomp.Application/DTOs/NoteRequestTo.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Distcomp.Application.DTOs
+{
+    public record NoteRequestTo(
+        long? Id,
+        long IssueId,
+        string Content
+    );
+}
