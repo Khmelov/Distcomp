5e1ee5546bf7da4c3eef33994c878dfc9fae2497
Igor Zalivako <143809970+igorzalivako@users.noreply.github.com>
2026-02-20 12:10:59 +0300

    Auto-merge PR #582 (Automated)
    Co-authored-by: Igor Zalivako <someaddress@yandex.ru>

---
+++ 351004/Zalivako/DistributedComputing/src/Application/Exceptions/EditorNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Exceptions
+{
+    public class EditorNotFoundException : NotFoundException
+    {
+        public EditorNotFoundException(string message, Exception inner) : base(message, inner) { }
+
+        public EditorNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ 351004/Zalivako/DistributedComputing/src/Application/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        Task<TEntity?> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+
+        Task<TEntity?> FindAsync(ISpecification<TEntity> spec, CancellationToken ct = default);
+        
+        Task<IEnumerable<TEntity>> FindAllAsync(ISpecification<TEntity> spec, CancellationToken ct = default);
+
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default);
+        
+        Task<int> CountAsync(ISpecification<TEntity> spec, CancellationToken ct = default);
+        
+        Task<bool> AnyAsync(ISpecification<TEntity> spec, CancellationToken ct = default);
+
+        Task<TEntity> AddAsync(TEntity entity, CancellationToken ct = default);
+        
+        Task<TEntity?> UpdateAsync(TEntity entity, CancellationToken ct = default);
+        
+        Task<TEntity?> DeleteAsync(TEntity entity, CancellationToken ct = default);
+    }
+}
