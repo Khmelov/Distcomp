808e5620faf41f5ada5c80b49cc79ceb122ff39b
Viktoryia Pulish <viktoryiapulish@gmail.com>
2026-03-03 14:12:37 +0300

    Add JPA task

--- 351003/Pulish/app/models/comment.py
+++ 351003/Pulish/app/models/comment.py
@@ -1,8 +1,18 @@
-from app.models.base import BaseEntity
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.db.database import Base
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.models.topic import Topic
 
 
-class Comment(BaseEntity):
-    def __init__(self, id: int, content: str, topic_id: int):
-        self.id = id
-        self.content = content
-        self.topic_id = topic_id
+class Comment(Base):
+    __tablename__ = "tbl_comment"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    content: Mapped[str] = mapped_column(String)
+    topic_id: Mapped[int] = mapped_column(
+        ForeignKey("tbl_topic.id", ondelete="CASCADE"))
+
+    topic: Mapped["Topic"] = relationship(back_populates="comments")
