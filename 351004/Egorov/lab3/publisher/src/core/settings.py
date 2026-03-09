8687ab8f721a13444736c5b769dd770b1381384a
Aleeg0 <Sankafoxe@gmail.com>
2026-03-09 21:14:38 +0300

    add publisher microservice

---
+++ 351004/Egorov/lab3/publisher/src/core/settings.py
@@ -0,0 +1,18 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str = Field(validation_alias="POSTGRES_HOST", default="localhost")
+    DB_PORT: int = Field(validation_alias="POSTGRES_PORT", default=5432, ge=1, le=65535)
+    DB_USER: str = Field(validation_alias="POSTGRES_USER", min_length=1)
+    DB_PASSWORD: str = Field(validation_alias="POSTGRES_PASSWORD", min_length=1)
+    DB_NAME: str = Field(validation_alias="POSTGRES_DB", min_length=1)
+
+    DEBUG: bool = Field(default=True)
+
+    @property
+    def get_database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+settings = Settings()
