b21cb8961bcdf1fbc5c1136b18f6b9001e7f03f5
Yahor Lashchenka <egor.lashchenko@gmail.com>
2026-03-13 15:32:18 +0300

    lab 2 done

--- 351002/Lashchenka/src/main/java/by/bsuir/distcomp/mapper/TweetMapper.java
+++ 351002/Lashchenka/src/main/java/by/bsuir/distcomp/mapper/TweetMapper.java
@@ -4,11 +4,16 @@ import by.bsuir.distcomp.dto.request.TweetRequestTo;
 import by.bsuir.distcomp.dto.response.TweetResponseTo;
 import by.bsuir.distcomp.entity.Tweet;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface TweetMapper {
+    @Mapping(target = "marks", ignore = true)
     Tweet toEntity(TweetRequestTo dto);
+
     TweetResponseTo toResponseDto(Tweet entity);
+
+    @Mapping(target = "marks", ignore = true)
     void updateEntityFromDto(TweetRequestTo dto, @MappingTarget Tweet entity);
 }
