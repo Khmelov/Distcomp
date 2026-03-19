72e6b16b9420f74e80015d446d2b76299807d6a8
IdkWhatToWriteHereOhManItWasAlreadyUsed <dimoon.amid05@gmail.com>
2026-03-19 17:02:28 +0300

    Отмена интеграции userver в проект

---
+++ 351004/Lazuta/services/userver-create-service.sh
@@ -0,0 +1,8 @@
+    REPO_URL="https://github.com/userver-framework/userver.git"
+    BRANCH="v2.15"
+    WORKDIR="/tmp/userver-create-service/$BRANCH"
+    if [ ! -d "$WORKDIR" ]; then
+        mkdir -p "$WORKDIR"
+        git clone -q --depth 1 --branch "$BRANCH" "$REPO_URL" "$WORKDIR"
+    fi
+    "$WORKDIR/scripts/userver-create-service" "$@"
