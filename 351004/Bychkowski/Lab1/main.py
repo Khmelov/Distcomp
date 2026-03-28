81706fcc4763534a62d97c79467d35e795679ced
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-28 17:10:23 +0300

    lab2

--- 351004/Bychkowski/Lab1/main.py
+++ 351004/Bychkowski/Lab1/main.py
@@ -3,15 +3,15 @@ from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from routers import router
 from exceptions import AppError, app_exception_handler, validation_exception_handler
+from database import init_db
 
-app = FastAPI(title="Task310 REST API")
+init_db()
+
+app = FastAPI(title="Task320 JPA REST API")
 
 app.include_router(router)
-
-# Регистрируем обработчики ошибок
 app.add_exception_handler(AppError, app_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 if __name__ == "__main__":
-    # Запуск сервера на порту 24110 согласно ТЗ
     uvicorn.run("main:app", host="0.0.0.0", port=24110, reload=True)
