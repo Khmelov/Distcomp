b7e0c8c744425f6e0ac254ac121ae08b2456cc3f
Exponenta <the.exponenta@gmail.com>
2026-03-28 03:57:36 +0300

    Lab 1-2

---
+++ 351003/Naskevich/src/database/repositories/__init__.py
@@ -0,0 +1,11 @@
+from .editor import EditorRepository
+from .marker import MarkerRepository
+from .post import PostRepository
+from .tweet import TweetRepository
+
+__all__ = [
+    "EditorRepository",
+    "TweetRepository",
+    "MarkerRepository",
+    "PostRepository",
+]
