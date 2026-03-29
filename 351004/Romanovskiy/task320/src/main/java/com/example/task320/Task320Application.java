5082f87705ba2e22ec69286ef1d613ebc6fd5d23
matveq77 <matveuxdqwerty@gmail.com>
2026-03-30 00:07:12 +0300

    done 2 3

---
+++ 351004/Romanovskiy/task320/src/main/java/com/example/task320/Task320Application.java
@@ -0,0 +1,13 @@
+package com.example.task320;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task330Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task330Application.class, args);
+	}
+
+}
