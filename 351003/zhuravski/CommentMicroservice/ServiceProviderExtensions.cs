cbff7a3a81723d3e88e1d794eb63566aa7c27cc3
Vitali Zhuravski <vitaliycof@outlook.com>
2026-04-04 17:05:53 +0300

    Добавил ещё один обработчик.

--- 351003/zhuravski/CommentMicroservice/ServiceProviderExtensions.cs
+++ 351003/zhuravski/CommentMicroservice/ServiceProviderExtensions.cs
@@ -24,6 +24,7 @@ static internal class ServiceProviderExtensions
         collection.AddSingleton<IEventHandler, AddCommentHandler>();
         collection.AddSingleton<IEventHandler, DeleteCommentHandler>();
         collection.AddSingleton<IEventHandler, GetCommentHandler>();
+        collection.AddSingleton<IEventHandler, UpdateCommentHandler>();
 
         collection.AddSingleton<IEventOrchestratorService, EventOrchestratorService>();
         collection.AddSingleton<IEventProducerService, KafkaProducerService>();
