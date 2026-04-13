4cbba54f29bd9b7317260ce67d91f6752e8b1a6d
Maksim Kuchko <kuchkomaxim2527@gmail.com>
2026-04-13 12:14:28 +0300

    added services

--- 351004/Kuchko/ServerApp/ServerApp/Program.cs
+++ 351004/Kuchko/ServerApp/ServerApp/Program.cs
@@ -1,39 +1,55 @@
+using System.Reflection;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 using Scalar.AspNetCore;
-using ServerApp;
 using ServerApp.Infrastructure;
 using ServerApp.Models.DTOs;
+using ServerApp.Models.Entities;
+using ServerApp.Repository;
+using ServerApp.Services.Implementations;
+using ServerApp.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Настройка контроллеров с нашим префиксом
+var config = TypeAdapterConfig.GlobalSettings;
+config.Scan(Assembly.GetExecutingAssembly());
+
+builder.Services.AddSingleton<IRepository<Author>, InMemoryRepository<Author>>();
+builder.Services.AddSingleton<IRepository<Article>, InMemoryRepository<Article>>();
+builder.Services.AddSingleton<IRepository<Message>, InMemoryRepository<Message>>();
+builder.Services.AddSingleton<IRepository<Sticker>, InMemoryRepository<Sticker>>();
+
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IArticleService, ArticleService>();
+builder.Services.AddScoped<IMessageService, MessageService>();
+builder.Services.AddScoped<IStickerService, StickerService>();
+
 builder.Services.AddControllers(options =>
-{
-    options.Conventions.Add(new ApiPrefixConvention(new RouteAttribute("api/v1.0")));
-    options.Filters.Add<GlobalExceptionFilter>();
-});
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = context =>
     {
-        var errorMsg = string.Join(" | ", context.ModelState.Values
-            .SelectMany(v => v.Errors)
-            .Select(e => e.ErrorMessage));
+        options.Conventions.Add(new ApiPrefixConvention(new RouteAttribute("api/v1.0")));
+        options.Filters.Add<GlobalExceptionFilter>();
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var errorMsg = string.Join(" | ", context.ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage));
 
-        return new BadRequestObjectResult(new ErrorResponse(errorMsg, 40001));
-    };
-});
+            return new BadRequestObjectResult(new ErrorResponse(errorMsg, 40001));
+        };
+    });
 
-builder.Services.AddOpenApi(); 
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     // Генерирует эндпоинт с JSON описанием API (по умолчанию /openapi/v1.json)
-    app.MapOpenApi(); 
-    
+    app.MapOpenApi();
+
     app.MapScalarApiReference(options =>
     {
         options
@@ -46,4 +62,19 @@ if (app.Environment.IsDevelopment())
 // app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var authorRepo = scope.ServiceProvider.GetRequiredService<IRepository<Author>>();
+
+    if (!authorRepo.GetAll().Any())
+        authorRepo.Create(new Author
+        {
+            Login = "kuchkomaxim2527@gmail.com",
+            Password = "password123",
+            Firstname = "Максим",
+            Lastname = "Кучко"
+        });
+}
+
 app.Run();
