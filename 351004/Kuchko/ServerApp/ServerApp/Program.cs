9dd3aaccff5f9364670717b72970bdc0c0f8f304
Maksim Kuchko <kuchkomaxim2527@gmail.com>
2026-04-13 04:39:14 +0300

    add structure

--- 351004/Kuchko/ServerApp/ServerApp/Program.cs
+++ 351004/Kuchko/ServerApp/ServerApp/Program.cs
@@ -1,41 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Scalar.AspNetCore;
+using ServerApp;
+using ServerApp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// 1. Настройка контроллеров с нашим префиксом
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(new ApiPrefixConvention(new RouteAttribute("api/v1.0")));
+});
+
+builder.Services.AddOpenApi(); 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    // Генерирует эндпоинт с JSON описанием API (по умолчанию /openapi/v1.json)
+    app.MapOpenApi(); 
+    
+    app.MapScalarApiReference(options =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        options
+            .WithTitle("My Project API v1.0")
+            .WithTheme(ScalarTheme.DeepSpace);
+    });
 }
+
+
+// app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
