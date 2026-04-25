3b8cf295237b0eb84a34bbf3c6476f20a4558c22
matveq77 <matveuxdqwerty@gmail.com>
2026-04-25 18:26:34 +0300

    lab6 setup

---
+++ 351004/Romanovskiy/task361/publisher/src/main/java/com/example/task361/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.example.task361.repository;
+
+import com.example.task361.domain.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {}
