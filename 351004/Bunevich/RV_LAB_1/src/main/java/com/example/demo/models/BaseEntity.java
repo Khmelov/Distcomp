949a69aca3d8c16da255ae1e3fe0d11a02e1afe8
Polina <polina.bunevich.06@mail.ru>
2026-04-27 16:18:33 +0300

    add Lab2 and lab3

--- 351004/Bunevich/RV_LAB_1/src/main/java/com/example/demo/models/BaseEntity.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.models;
-
-import lombok.Data;
-
-@Data
-public class BaseEntity {
-    protected Long id;
-}
--- 351004/Bunevich/RV_LAB_1/src/main/java/com/example/demo/models/Post.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.models;
-
-import lombok.Data;
-
-@Data
-public class Post extends BaseEntity{
-    public long storyId;
-    public String content;
-}
