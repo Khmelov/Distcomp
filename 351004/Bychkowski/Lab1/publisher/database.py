310c21918a86631fd4f14e8b52c74b9e6b46abc4
pavel.bychkouski <ipasha1337@yandex.by>
2026-04-30 10:22:18 +0300

    labs 3 4 5 6

--- 351004/Bychkowski/Lab1/publisher/database.py
+++ 351004/Bychkowski/Lab1/publisher/database.py
@@ -7,16 +7,11 @@ engine = create_engine(DB_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
 def init_db():
-    Base.metadata.drop_all(bind=engine)
-
-    with engine.connect() as connection:
-        connection.execute(text("CREATE SCHEMA IF NOT EXISTS distcomp"))
-        connection.commit()
-
+    with engine.begin() as conn:
+        conn.execute(text("DROP SCHEMA IF EXISTS distcomp CASCADE"))
+        conn.execute(text("CREATE SCHEMA distcomp"))
     Base.metadata.create_all(bind=engine)
-
 
 def get_db():
     db = SessionLocal()
